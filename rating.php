975e84b7d3bfb8fbc65bf75cd97cf0676b8acd22
CementTheBlock <cement122293@gmail.com>
2018-06-20 13:03:03 -0400

    more bug huntin

--- rating.php
+++ rating.php
@@ -6,28 +6,28 @@ if(isset($_POST['team1_player1'], $_POST['team1_player2'], $_POST['team1_player3
     $result = $_POST['result'];
 
     $db = new PDO(JAYNE_CON . JAYNE_DB, JAYNE_DB_USER, JAYNE_DB_PASS, $opt);
-    $do = $db->prepare('INSERT INTO MatchHistory (team1_player1, team1_player2, team1_player3, team1_player4, team1_player5, team1_player6, team2_player1, team2_player2, team2_player3, team2_player4, team2_player5, team2_player6, result)
-                        VALUES                   (:team1_player1, :team1_player2, :team1_player3, :team1_player4, :team1_player5, :team1_player6, :team2_player1, :team2_player2, :team2_player3, :team2_player4, :team2_player5, :team2_player6, :result)');
+    // $do = $db->prepare('INSERT INTO MatchHistory (team1_player1, team1_player2, team1_player3, team1_player4, team1_player5, team1_player6, team2_player1, team2_player2, team2_player3, team2_player4, team2_player5, team2_player6, result)
+                        // VALUES                   (:team1_player1, :team1_player2, :team1_player3, :team1_player4, :team1_player5, :team1_player6, :team2_player1, :team2_player2, :team2_player3, :team2_player4, :team2_player5, :team2_player6, :result)');
 
-    $do->bindValue(':team1_player1', $_POST['team1_player1'], PDO::PARAM_INT);
-    $do->bindValue(':team1_player2', $_POST['team1_player2'], PDO::PARAM_INT);
-    $do->bindValue(':team1_player3', $_POST['team1_player3'], PDO::PARAM_INT);
-    $do->bindValue(':team1_player4', $_POST['team1_player4'], PDO::PARAM_INT);
-    $do->bindValue(':team1_player5', $_POST['team1_player5'], PDO::PARAM_INT);
-    $do->bindValue(':team1_player6', $_POST['team1_player6'], PDO::PARAM_INT);
-    $do->bindValue(':team2_player1', $_POST['team2_player1'], PDO::PARAM_INT);
-    $do->bindValue(':team2_player2', $_POST['team2_player2'], PDO::PARAM_INT);
-    $do->bindValue(':team2_player3', $_POST['team2_player3'], PDO::PARAM_INT);
-    $do->bindValue(':team2_player4', $_POST['team2_player4'], PDO::PARAM_INT);
-    $do->bindValue(':team2_player5', $_POST['team2_player5'], PDO::PARAM_INT);
-    $do->bindValue(':team2_player6', $_POST['team2_player6'], PDO::PARAM_INT);
-    $do->bindValue(':result', $result, PDO::PARAM_INT);
+    // $do->bindValue(':team1_player1', $_POST['team1_player1'], PDO::PARAM_INT);
+    // $do->bindValue(':team1_player2', $_POST['team1_player2'], PDO::PARAM_INT);
+    // $do->bindValue(':team1_player3', $_POST['team1_player3'], PDO::PARAM_INT);
+    // $do->bindValue(':team1_player4', $_POST['team1_player4'], PDO::PARAM_INT);
+    // $do->bindValue(':team1_player5', $_POST['team1_player5'], PDO::PARAM_INT);
+    // $do->bindValue(':team1_player6', $_POST['team1_player6'], PDO::PARAM_INT);
+    // $do->bindValue(':team2_player1', $_POST['team2_player1'], PDO::PARAM_INT);
+    // $do->bindValue(':team2_player2', $_POST['team2_player2'], PDO::PARAM_INT);
+    // $do->bindValue(':team2_player3', $_POST['team2_player3'], PDO::PARAM_INT);
+    // $do->bindValue(':team2_player4', $_POST['team2_player4'], PDO::PARAM_INT);
+    // $do->bindValue(':team2_player5', $_POST['team2_player5'], PDO::PARAM_INT);
+    // $do->bindValue(':team2_player6', $_POST['team2_player6'], PDO::PARAM_INT);
+    // $do->bindValue(':result', $result, PDO::PARAM_INT);
 
-    try {
-        $do->execute();
-    } catch (PDOException $e) {
-        die("Error updating Match History: " . $e->getMessage());
-    }
+    // try {
+        // $do->execute();
+    // } catch (PDOException $e) {
+        // die("Error updating Match History: " . $e->getMessage());
+    // }
 
     $team1 = array( $_POST['team1_player1'], $_POST['team1_player2'], $_POST['team1_player3'], $_POST['team1_player4'], $_POST['team1_player5'], $_POST['team1_player6']);
     $team2 = array( $_POST['team2_player1'], $_POST['team2_player2'], $_POST['team2_player3'], $_POST['team2_player4'], $_POST['team2_player5'], $_POST['team2_player6']);
