5bbe4032658ed37b259fd1b212d48a5c16e1c73e
svenvv <github@svenvv.nl>
2018-06-20 23:44:19 +0200

    Actually making the thing work

--- rating.php
+++ rating.php
@@ -1,92 +1,82 @@
 <?php
-if(isset($_POST['team1_player1'], 
-         $_POST['team1_player2'], 
-         $_POST['team1_player3'], 
-         $_POST['team1_player4'], 
-         $_POST['team1_player5'], 
-         $_POST['team1_player6'], 
-         $_POST['team2_player1'], 
-         $_POST['team2_player2'], 
-         $_POST['team2_player3'], 
-         $_POST['team2_player4'], 
-         $_POST['team2_player5'], 
-         $_POST['team2_player6'], 
+if(isset($_POST['team1_player1'],
+         $_POST['team1_player2'],
+         $_POST['team1_player3'],
+         $_POST['team1_player4'],
+         $_POST['team1_player5'],
+         $_POST['team1_player6'],
+         $_POST['team2_player1'],
+         $_POST['team2_player2'],
+         $_POST['team2_player3'],
+         $_POST['team2_player4'],
+         $_POST['team2_player5'],
+         $_POST['team2_player6'],
          $_POST['result']))
 {
     require_once  "/opt/dbsettings.php";
     $result   = $_POST['result'];
-    $db       = new PDO(JAYNE_CON . JAYNE_DB, JAYNE_DB_USER, 
-                        JAYNE_DB_PASS, $opt);
-    // $do = $db->prepare('INSERT INTO MatchHistory (team1_player1, team1_player2, team1_player3, team1_player4, team1_player5, team1_player6, team2_player1, team2_player2, team2_player3, team2_player4, team2_player5, team2_player6, result)
-                        // VALUES                   (:team1_player1, :team1_player2, :team1_player3, :team1_player4, :team1_player5, :team1_player6, :team2_player1, :team2_player2, :team2_player3, :team2_player4, :team2_player5, :team2_player6, :result)');
+    $db = new PDO(JAYNE_CON . JAYNE_DB, JAYNE_DB_USER, JAYNE_DB_PASS, $opt); 
 
-    // $do->bindValue(':team1_player1', $_POST['team1_player1'], PDO::PARAM_INT);
-    // $do->bindValue(':team1_player2', $_POST['team1_player2'], PDO::PARAM_INT);
-    // $do->bindValue(':team1_player3', $_POST['team1_player3'], PDO::PARAM_INT);
-    // $do->bindValue(':team1_player4', $_POST['team1_player4'], PDO::PARAM_INT);
-    // $do->bindValue(':team1_player5', $_POST['team1_player5'], PDO::PARAM_INT);
-    // $do->bindValue(':team1_player6', $_POST['team1_player6'], PDO::PARAM_INT);
-    // $do->bindValue(':team2_player1', $_POST['team2_player1'], PDO::PARAM_INT);
-    // $do->bindValue(':team2_player2', $_POST['team2_player2'], PDO::PARAM_INT);
-    // $do->bindValue(':team2_player3', $_POST['team2_player3'], PDO::PARAM_INT);
-    // $do->bindValue(':team2_player4', $_POST['team2_player4'], PDO::PARAM_INT);
-    // $do->bindValue(':team2_player5', $_POST['team2_player5'], PDO::PARAM_INT);
-    // $do->bindValue(':team2_player6', $_POST['team2_player6'], PDO::PARAM_INT);
-    // $do->bindValue(':result', $result, PDO::PARAM_INT);
-
-    // try {
-        // $do->execute();
-    // } catch (PDOException $e) {
-        // die("Error updating Match History: " . $e->getMessage());
-    // }
-
-    $team1    = array($_POST['team1_player1'], 
-                      $_POST['team1_player2'], 
-                      $_POST['team1_player3'], 
-                      $_POST['team1_player4'], 
-                      $_POST['team1_player5'], 
+    $team1    = array($_POST['team1_player1'],
+                      $_POST['team1_player2'],
+                      $_POST['team1_player3'],
+                      $_POST['team1_player4'],
+                      $_POST['team1_player5'],
                       $_POST['team1_player6']);
-    $team2    = array($_POST['team2_player1'], 
-                      $_POST['team2_player2'], 
-                      $_POST['team2_player3'], 
-                      $_POST['team2_player4'], 
-                      $_POST['team2_player5'], 
+					  
+    $team2    = array($_POST['team2_player1'],
+                      $_POST['team2_player2'],
+                      $_POST['team2_player3'],
+                      $_POST['team2_player4'],
+                      $_POST['team2_player5'],
                       $_POST['team2_player6']);
     $lobby    = array_merge($team1, $team2);
-    $team_ids = implode(',', array_fill(0, count($team1), '?'));
-    $ids      = $team1_ids . $team2_ids;
-    //Add players not already in DB to DB
-    for ($i = 0, $lobby_size = count ($lobby); $i < $lobby_size; $i++)
+
+    $lobby_sql_helper  = str_repeat('?,', count($lobby) - 1) . '?';
+    $team_sql_helper  = str_repeat('?,', count($team1) - 1) . '?';
+
+
+    
+//Add players not already in DB to DB
+    $sql="INSERT INTO Main(discord_id, name) VALUES (?,?) ON DUPLICATE KEY UPDATE discord_id = discord_id;";
+    for ($i = 0 ; $i < count($lobby) ; $i++)
     {
-        $discord_id = $lobby[$i];
-        $do         = $db->prepare("INSERT INTO Main 
-                                    (discord_id, name, rating, wins, losses, draws)
-                                    VALUES 
-                                    (:discordID, :name, :rating, :wins, :losses, :draws)
-                                    ON DUPLICATE KEY UPDATE
-                                      rating = rating");
-        $do->bindValue(':discordID', $discord_id, PDO::PARAM_INT);
-        $do->bindValue(':name', "", PDO::PARAM_STR);
-        $do->bindValue(':rating', 1600, PDO::PARAM_INT);
-        $do->bindValue(':wins', 0, PDO::PARAM_INT);
-        $do->bindValue(':losses', 0, PDO::PARAM_INT);
-        $do->bindValue(':draws', 0, PDO::PARAM_INT);
+        try {
+                $do = $db->prepare($sql);
+                if(!is_numeric($lobby[$i])) die("lobby[$i] is not a number!");
+                $do->execute([$lobby[$i],""]);
+        } catch (PDOException $e) {
+                die ("Error Updating Match History: " . $e->getMessage());
+                }
     }
+	//add to match history
+	$sql = <<<MARKER
+INSERT INTO MatchHistory 
+(Match_ID, WinningTeam, TimeStamp, Discord_ID1, Discord_ID2, Discord_ID3, Discord_ID4, Discord_ID5, Discord_ID6, Discord_ID7, Discord_ID8, Discord_ID9, Discord_ID10, Discord_ID11, Discord_ID12) 
+VALUES (NULL, ?, CURRENT_TIMESTAMP, ?,?,?,?,?,?,?,?,?,?,?,?)
+MARKER;
+
+	$do  = $db->prepare($sql);
+        try {
+            $do->execute([$result,$team1[0],$team1[1],$team1[2],$team1[3],$team1[4],$team1[5],$team2[0],$team2[1],$team2[2],$team2[3],$team2[4],$team2[5]]);
+        } catch (PDOException $e) {
+            die ("Error Updating Match History: " . $e->getMessage());
+        }
+
     //Fetch ratings for each and define ratings//
-    $sql  = "SELECT rating
-FROM Main
-WHERE discord_id IN ({[$ids]})
-ORDER BY FIELD(discord_id, [{[$ids]}]";
-    $stmt = $db->prepare($sql);
-    try {
-        $do->execute(array_merge($lobby, $lobby));
-    } catch (PDOException $e) {
-        die ("Error Fetching Ratings: " . $e->getMessage());
-    }
-    $ratings = $stmt->fetchAll(FETCH_COLUMN, 0);
-    //Variables - 12 ID's, 12 Ratings
-    //----------------------------------------------------------------------------
-    //Find each team's avg
+    $sql  = "SELECT rating FROM Main WHERE discord_id IN ($lobby_sql_helper) ORDER BY FIELD(discord_id,$lobby_sql_helper)";
+	try
+{
+	$do  = $db->prepare($sql);
+             $do->execute(array_merge($lobby,$lobby));
+        } catch (PDOException $e) {
+            die ("Error Updating Match History: " . $e->getMessage());
+        }
+    $ratings = $do ->fetchAll(PDO::FETCH_COLUMN, 0);
+
+//Variables - 12 ID's, 12 Ratings
+//----------------------------------------------------------------------------
+//Find each team's avg
     $teamrating_1   = ((array_sum(array_slice($ratings, 0, 6)))/6);
     $teamrating_2   = ((array_sum(array_slice($ratings, 6, 6)))/6);
     $K_CONST         = 32;
@@ -101,64 +91,64 @@ ORDER BY FIELD(discord_id, [{[$ids]}]";
     //Changes Calculated
     //---------------------------------------------------------------
     //Changing Rating and Wins/Losses/Draws
-    if($result=0){
+
+
+    if($result == 0){
         //Add a draw to all twelve players
-        $sql = "UPDATE Main SET draws = draws+1 WHERE discord_id IN({[$ids]})";
+        $sql = "UPDATE Main SET draws = draws+1 WHERE discord_id IN ($lobby_sql_helper)";
         $do  = $db->prepare($sql);
         try {
             $do->execute($lobby);
         } catch (PDOException $e) {
             die ("Error Updating Draw Ratings: " . $e->getMessage());
         }
-        header("Location: /Form.html");
+        header("Location: Form.html");
         die ("Successful Update.");
-    }else if($result = 1){
+    }
+	else if($result == 1){
         //Add a win to players 1-6 and a loss to players 7-12
         //Add $varwin1 to rating of players 1-6, Add $varloss2 to rating of players 7-12
-        $sql = "UPDATE Main 
-                SET wins = wins+1, rating = rating+$varwin1 
-                WHERE discord_id IN({[$team_ids]})";
+        $sql = "UPDATE Main SET wins = wins+1, rating = rating+$varwin1 WHERE discord_id IN ($team_sql_helper)";
         $do  = $db->prepare($sql);
         try {
             $do->execute($team1);
         } catch (PDOException $e) {
             die ("Error Updating team1 win Ratings: " . $e->getMessage());
         }
-        $sql = "UPDATE Main 
-                SET losses = losses+1, rating+$varloss2
-                WHERE discord_id IN({[$team_ids]})";
+        $sql = "UPDATE Main SET losses = losses+1, rating+$varloss2 WHERE discord_id IN($team_sql_helper)";
         $do  = $db->prepare($sql);
         try {
             $do->execute($team2);
         } catch (PDOException $e) {
             die ("Error Updating team2 loss Ratings: " . $e->getMessage());
         }
-        header("Location: /Form.html");
+        header("Location: Form.html");
         die ("Successful Update.");
-    }else if($result = 2){
-        //Add a win to players 7-12, and a loss to players 1-6
+		
+		
+    }else if($result == 2){
+       //Add a win to players 7-12, and a loss to players 1-6
         //Add $varloss1 to rating of players 1-6, Add $varwin2 to rating of players 7-12
-        $sql = "UPDATE Main 
-                Set losses = losses+1, rating = rating+$varloss1 
-                WHERE discord_id IN({[$team_ids]})";
+        $sql = "UPDATE Main SET losses = losses+1, rating = rating+$varloss1 WHERE discord_id IN($team_sql_helper)";
         $do  = $db->prepare($sql);
         try {
             $do->execute($team1);
         } catch (PDOException $e) {
             die ("Error Updating team2 win Ratings: " . $e->getMessage());
         }
-        $sql = "UPDATE Main 
-                Set wins = wins+1, rating = rating+$varwin2 
-                WHERE discord_id IN({[$team_ids]})";
+        $sql = "UPDATE Main SET wins = wins+1, rating = rating+$varwin2 WHERE discord_id IN($team_sql_helper)";
         $do  = $db->prepare($sql);
         try{
             $do->execute($team2);
         } catch (PDOException $e) {
             die ("Error Updating team1 loss Ratings: " . $e->getMessage());
         }
-        header("Location: /Form.html");
+		
+		
+        header("Location: Form.html");
         die ("Successful Update");
     }
+
 } else {
     die( "Something is not right" );
 }
