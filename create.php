e681ff335a2c15d0bcb5440b796d5ddb450b68eb
ohwhy <me@0day.cat>
2018-06-20 11:29:37 +0200

    Update create.php

--- create.php
+++ create.php
@@ -1,22 +1,25 @@
 <?php
-require_once  "/opt/dbsettings.php";
+if(isset($_POST['name'], $_POST['discordID']))
+{
+    require_once  "/opt/dbsettings.php";
+    $db = new PDO(JAYNE_CON . JAYNE_DB, JAYNE_DB_USER, JAYNE_DB_PASS, $opt);
+    $do = $db->prepare('INSERT INTO Main (discord_id, name, rating, wins, losses, draws)
+                        VALUES (:discordID, :name, :rating, :wins, :losses, :draws)');
 
-$db = new PDO(JAYNE_CON . JAYNE_DB, JAYNE_DB_USER, JAYNE_DB_PASS, $opt);
+    $do->bindValue(':discordID', $_POST['discordID'], PDO::PARAM_INT);
+    $do->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+    $do->bindValue(':rating', $_POST['rating'] , PDO::PARAM_INT);
+    $do->bindValue(':wins', $_POST['wins'] , PDO::PARAM_INT);
+    $do->bindValue(':losses', $_POST['losses'], PDO::PARAM_INT);
+    $do->bindValue(':draws', $_POST['draws'], PDO::PARAM_INT);
 
-$discordID = 87593699638280192;
-$name = 'Smug';
-
-
-$do = $db->prepare('INSERT INTO Main (discord_id, name, rating, wins, losses, draws)
-                    VALUES (:discordID, :name, :rating, :wins, :losses, :draws)');
-
-$do->bindValue(':discordID', $discordID, PDO::PARAM_INT);
-$do->bindValue(':name', $name, PDO::PARAM_STR);
-$do->bindValue(':rating', 5000 , PDO::PARAM_INT);
-$do->bindValue(':wins', 100 , PDO::PARAM_INT);
-$do->bindValue(':losses', 0, PDO::PARAM_INT);
-$do->bindValue(':draws', 0, PDO::PARAM_INT);
-
-$do->execute();
-echo "Executed!";
+    try {
+        $do->execute();
+        echo "Executed!";
+    } catch (PDOException $e) {
+        die("Error updating leaderboards: " . $e->getMessage());
+    }
+} else {
+    die("Something is not right.");
+}
 ?>
