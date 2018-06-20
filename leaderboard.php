a9814b724fd3a8b4f0998c285a5de0971b5bb6e9
Miguel Robledo <migroble@ucm.es>
2018-06-20 19:17:32 +0200

    leaderboard should display better on mobile

--- leaderboard.php
+++ leaderboard.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet" type="text/css" href="css/leaderboard.css">
 	</head>
 	<body>
-		<table id="leaderboard" class="table table-hover" align="center">
+		<table id="leaderboard" class="table table-hover">
 			<thead class="thead-dark">
 				<tr>
 					<th>Rank</th>
