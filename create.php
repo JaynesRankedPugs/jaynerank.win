6e4582be8b2950f9c0bb2181448d026b3519204d
CementTheBlock <cement122293@gmail.com>
2018-06-20 09:06:27 -0400

    bug finding

--- create.php
+++ create.php
@@ -6,10 +6,11 @@ if(isset($_POST['name'], $_POST['discordID']))
     $do = $db->prepare("INSERT INTO Main (discord_id, name, rating, wins, losses, draws)
                         VALUES (:discordID, :name, :rating, :wins, :losses, :draws)
                         ON DUPLICATE KEY UPDATE
-                            name = :name;");
+                            name = :dup_name;");
 
     $do->bindValue(':discordID', $_POST['discordID'], PDO::PARAM_INT);
     $do->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+    $do->bindValue(':dup_name', $_POST['name'], PDO::PARAM_STR);
     $do->bindValue(':rating', $_POST['rating'] , PDO::PARAM_INT);
     $do->bindValue(':wins', $_POST['wins'] , PDO::PARAM_INT);
     $do->bindValue(':losses', $_POST['losses'], PDO::PARAM_INT);
