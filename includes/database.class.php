d7f46f45a30c595fa40f9c4661807818e2226344
$mug <me@0day.cat>
2018-06-30 18:54:08 +0200

    Progress on OOP, also adding in filters and search function to leaderboards

--- includes/database.class.php
+++ includes/database.class.php
@@ -1,9 +1,8 @@
 <?php
 
-
 class Database
 {
-    private $conn;
+    private $_conn;
     private $_db;
     private $_user;
     private $_pass;
@@ -11,50 +10,87 @@ class Database
 
     public function __construct($ENV)
     {
-        require_once ('/opt/main.inc.php');
+        include_once '/opt/main.inc.php';
         $this->_user = JAYNE_DB_USER;
         $this->_pass = JAYNE_DB_PASS;
         $this->_host = JAYNE_DB_HOST;
-                
+
         if($ENV == "dev") $this->_db = JAYNE_DB_DEV;
         else $this->_db = JAYNE_DB_LIVE;
         
         try {
-            $this->conn = 
-                new PDO($this->_host . $this->_db, 
-                        $this->_user, $this->_pass,
-                        [   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                            PDO::ATTR_EMULATE_PREPARES => false
-                        ]
-                    );        
+            $this->_conn 
+                = new PDO(
+                    $this->_host . $this->_db, 
+                    $this->_user, $this->_pass,
+                    [   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                        PDO::ATTR_EMULATE_PREPARES => false
+                    ]
+                );        
         } catch (PDOException $e) {
             die('Connection failed: '.$e->getMessage());
         }
     }
 
-    private $resp;
-    public function getLeaderboard($style)
+    /**
+     * Returns leaderboard under spesificed conditions
+     *   
+     * @param boolean $noGames Filtered list or not 
+     * @param string  $player  User to look up
+     *
+     * @return string  Return leaderboard    
+     */
+    public $resp;
+    public function getLeaderboard($noGames, $player)
     {
-        if($style == "full") $nofgames = 0;
-        else $nofgames = 4;
+        $this->noGame = filter_var($noGames, FILTER_VALIDATE_BOOLEAN);
+        $this->player = $player.'%';
 
-        $do = $this->conn->prepare("SELECT * FROM `Main` WHERE (wins+losses+draws) > $nofgames ORDER by `rating` DESC");
+        $col = "@curRank := @curRank + 1 AS rank, name, rating, wins, losses, draws";
+        $do = $this->_conn->prepare(
+            "SELECT $col FROM `Main` m, (SELECT @curRank := 0) r ".
+            "ORDER by `rating` DESC"
+        );  
+
+        /*
+        if ($this->player) {
+            // Filtered search by name              
+            $do = $this->_conn->prepare(
+                "SELECT $col FROM `Main` m, (SELECT @curRank := 0) r ".
+                "WHERE name like :player ORDER BY `rating` DESC;"
+            );
+            $do->bindParam(":player", $this->player);
+        
+        } else {
+            // Filtered
+            $do = $this->_conn->prepare(
+                "SELECT $col FROM `Main` m, (SELECT @curRank := 0) r ".
+                "ORDER by `rating` DESC"
+            );                
+        }
+        */
+        
         $do->execute();
-        $ranking = 0;
+        $rank = 0;
+        $this->resp = "";
         while ($row = $do->fetch(PDO::FETCH_ASSOC)) {
-            $ranking += 1;
-            $winrate = round(100 * $row["wins"] / ($row["wins"] + $row["losses"] + $row["draws"]), 1);
+            $rank += 1;
+            $total = $row["wins"] + $row["losses"] + $row["draws"];
+            $winrate = round(100 * $row["wins"] / ($total), 1);
             $winrate = (is_nan($winrate) ? 0 : $winrate);
-            $this->resp .= "<tr>";
-            $this->resp .= "\t<th scope=\"row\">#".$ranking."</th>";
-            $this->resp .= "\t<td>".$row["name"]."</td>";
-            $this->resp .= "\t<td>".$row["rating"]."</td>";
-            $this->resp .= "\t<td>".$row["wins"]."</td>";
-            $this->resp .= "\t<td>".$row["losses"]."</td>";
-            $this->resp .= "\t<td>".$row["draws"]."</td>";
-            $this->resp .= "\t<td>".$winrate."%</td>";
-            $this->resp .= "</tr>";
+            if ($this->noGame == 1 || $total > 0) {         
+                $this->resp .= "<tr>";
+                $this->resp .= "\t<th scope=\"row\">#".$row["rank"]."</th>";
+                $this->resp .= "\t<td>".$row["name"]."</td>";
+                $this->resp .= "\t<td>".$row["rating"]."</td>";
+                $this->resp .= "\t<td>".$row["wins"]."</td>";
+                $this->resp .= "\t<td>".$row["losses"]."</td>";
+                $this->resp .= "\t<td>".$row["draws"]."</td>";
+                $this->resp .= "\t<td>$winrate%</td>";
+                $this->resp .= "\t<td>".$total."</td>";
+                $this->resp .= "</tr>";
+            }
         }
         return $this->resp;
     }
