b536a2518eba8d6843f08859f07ff71f868aee79
$mug <me@0day.cat>
2018-07-02 18:47:41 +0200

    Woops
    kinda ruined the  phone support but that's fixed now :)

--- public/index.php
+++ public/index.php
@@ -17,7 +17,10 @@ if (isset($_SESSION['username'])) {
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="/">Jayne's Ranked PUGs</a>
-    <div class="collapse navbar-collapse" id="navbarText">
+    <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
